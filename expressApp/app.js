9ea9e96646198efbf288f53094bdb7fb9eb87bca
finneganthefine <finneganthefine@gmail.com>
2023-10-30 11:53:05 -0600

    It's Working

--- expressApp/app.js
+++ expressApp/app.js
@@ -2,28 +2,35 @@ const express = require('express')
 const app = express()
 const port = 3000
 const mysql = require('mysql')
+require('dotenv').config()
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'password',
-  database: 'shark',
-  port: '3306'
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASS,
+  database: 'sharkdb',
+  port: process.env.PORT
 })
 
 connection.connect()
 
-connection.query('SELECT 1 + 1 AS solution', (err, rows, fields) => {
-  if (err) throw err
+var myBook = null
+const user = 0;
 
-  console.log('The solution is: ', rows[0].solution)
+connection.query('SELECT * from user', (err, rows, fields) => {
+  if (err) throw err
+  myBook = rows
+  console.log('The solution is: ', rows)
 })
 
+myBook = JSON.parse(myBook)
+
+app.get('/', (req, res) => {
+  const name = myBook[0]
+  //res.send('Hello World!')
+  res.send(name.username)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
 connection.end()
-
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
-
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`)
-// })
